e26a58905848db2d4d0a81f543903dfbeef8e271
wasupalonely <juandiegomaciasoy@gmail.com>
2024-08-08 17:32:03 -0500

    bug: adding config files but the bug persists

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+import {Text} from 'react-native';
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Text>Hola!</Text>
+    </NavigationContainer>
+  );
+};
+
+export default App;
